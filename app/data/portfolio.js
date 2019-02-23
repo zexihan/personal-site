cd6e7098d549b44088fea952b27db5c5b28b45c7
Zexi Han <zexihan@me.com>
2019-02-23 00:03:38 -0500

    update

--- app/data/portfolio.js
+++ app/data/portfolio.js
@@ -4,7 +4,7 @@ const data = [
     title: 'Nearest Dollar',
     subtitle: '2015 BVP Hackathon',
     link: '/portfolio/nearestdollar/',
-    image: '/images/projects/nearestdollar.jpg',
+    image: '/images/portfolio/nearestdollar.jpg',
     date: '2015-11-20',
     desc: 'Built for a social impact hackathon. '
       + 'NearestDollar connected to your bank accounts, credit cards, '
@@ -14,7 +14,7 @@ const data = [
     title: 'Harvest',
     subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
     link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
+    image: '/images/portfolio/harvest.jpg',
     date: '2015-09-20',
     desc: 'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
       + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
@@ -23,7 +23,7 @@ const data = [
     title: 'Space Potato',
     subtitle: 'A kickstarter funded potato powered weather balloon.',
     link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
+    image: '/images/portfolio/spacepotato.jpg',
     date: '2015-06-28',
     desc: 'Launched a potato battery powered weather balloon with two cameras '
       + 'and gps transponder. Resulting photos were published in a coffee table book. '
@@ -32,7 +32,7 @@ const data = [
     title: 'Cat Detector',
     subtitle: 'A convolutional neural network to classify cats! (and dogs)',
     link: '/project/catdetector/',
-    image: '/images/projects/catdetector.jpg',
+    image: '/images/portfolio/catdetector.jpg',
     date: '2015-05-15',
     desc: 'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
      + 'Over 60,000 cats were classified before server bills made the project too expensive '
