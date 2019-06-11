12e14bb8f44fbe2e744d54a1db1d9125a836fae0
Zexi Han <zexihan@me.com>
2019-06-11 16:09:04 -0700

    Updated experience

--- app/data/resume/positions.js
+++ app/data/resume/positions.js
@@ -1,25 +1,26 @@
 
 const positions = [
   {
-    company: "Rue Gilt Groupe",
+    company: "End-to-End Machine Learning – Reseller Identification",
     position: "Data Scientist Co-op",
     link: "/documents/Reseller_Identification.pdf",
-    daterange: "January 2018 - June 2018",
+    daterange: "Rue La La, Rue Gilt Groupe, January 2018 - June 2018",
     points: [
-      "Worked on feature engineering and XGBoost model training from an iterative perspective to identify resellers from over 2 million buyers, and put it into production to provide them with personalized boutique recommendations.",
-      "Built docker apps for feature extraction, training and inference which were deployed to Amazon ECS and Airflow.",
-      "Maintained daily ETL process for the recommendation system with robust SQL on Snowflake."
+      "Developed an end-to-end learning system with Python and XGBoost model for personalized boutique recommendation.",
+      "Extracted features with SQL, NumPy, and Pandas on user profile, purchase history, and site activity data of 2M buyers.",
+      "Trained and tuned XGBoost model on iteratively-refined dataset to overcome the class imbalance.",
+      "Dockernized the system and deployed it onto Amazon ECS, Airflow, and Snowflake for automated ETL process."
     ]
   },
   {
-    company: "National Laboratory of Pattern Recognition",
-    position: "Research Assistant",
+    company: "Caffe-based Visual Search for Same-Style Product Images",
+    position: "Software Engineer Intern - AI",
     link: "http://zexihancs.weebly.com/imageretrieval.html",
-    daterange: "August 2015 - May 2016",
+    daterange: "Taobao, Alibaba Group, August 2015 - May 2016",
     points: [
-      "Designed and built a Three-stage Hybrid Visual Search Framework (Classification, Object Detection and Matching) to the task of same-style product image retrieval with convolutional neural networks.",
-      "Experimented on the Taobao 5 million product image dataset with multiple CNN models using Caffe.",
-      "Developed the backend of Android demo and achieved real-time same-style product image retrieval."
+      "Designed and built a three-stage hybrid visual search framework (classification, object detection, matching) based on ConvNet implemented with Caffe in C++, which awarded the Outstanding Final Project (Rank 12/680).",
+      "Pre-processed 5M images of 676 classes from Taobao.com using the imgproc module of OpenCV.",
+      "Fine-tuned AlexNet for image classification and Faster R-CNN for both object detection and feature extraction."
     ]
   }
 ];
